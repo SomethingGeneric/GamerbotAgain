734d7ee4bd2b05b90644a538f50fad13e355a021
Matt C <matthew.compton@mymail.champlain.edu>
2023-02-14 17:40:45 -0500

    should've done this earlier lmao

--- bot/src/exts/doesnotexist.py
+++ bot/src/exts/doesnotexist.py
@@ -18,18 +18,21 @@ class DoesNotExist(commands.Cog):
     @commands.slash_command()
     async def dalle(self, inter, *, prompt: str):
         """Image generator thingie"""
-        await inter.response.defer()
-        openai.api_key = config["openai_key"]
-        response = openai.Image.create(prompt=prompt, n=1, size="1024x1024")
-        image_url = response["data"][0]["url"]
-        r = requests.get(image_url)
+        try:
+            await inter.response.defer()
+            openai.api_key = config["openai_key"]
+            response = openai.Image.create(prompt=prompt, n=1, size="1024x1024")
+            image_url = response["data"][0]["url"]
+            r = requests.get(image_url)
 
-        fn = f"dalle-out-{str(randint(1,1000))}.png"
+            fn = f"dalle-out-{str(randint(1,1000))}.png"
 
-        open(fn, "wb").write(r.content)
+            open(fn, "wb").write(r.content)
 
-        await inter.send(f"You said: `{prompt}`.", file=disnake.File(fn))
-        os.remove(fn)
+            await inter.send(f"You said: `{prompt}`.", file=disnake.File(fn))
+            os.remove(fn)
+        except Exception as e:
+            await inter.send(f"Error: ```{str(e)}```")
 
 def setup(bot):
     print("Loading DNE ext")
